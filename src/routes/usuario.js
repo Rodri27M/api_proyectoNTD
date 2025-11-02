765a1c0885d079bcad4ed4aa6b0f60ba6913755a
PepeGrilloSalah <pipelgrisli@gmail.com>
2025-11-02 13:20:51 -0500

    metodo get

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -23,5 +23,17 @@ router.post("/", async (req, res) => {
     });
   }
 });
+// Obtener todos los usuarios
+router.get("/", async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+    res.status(200).json(usuarios);
+  } catch (error) {
+    res.status(500).json({
+      mensaje: "Error al obtener los usuarios",
+      error: error.message,
+    });
+  }
+});
 
 export default router;
